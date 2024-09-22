e7d2b25d2d050f6ee0a7a951bd22ff92c495222d
AidaTegeran <aidategeran@gmail.com>
2024-09-22 14:56:01 -0500

    create fibonacci function

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static HashMap<Integer, Long> cache;
+
+    static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        }
+
+        long fib = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            long fib = fibonacci(i);
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("Fib i = " + i + "=" + fib) ;
+        }
+    }
+}
